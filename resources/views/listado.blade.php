570f23a83623c5f6660e1a81bf648c7980a6f658
SirBeho <benjamin.tavarez.98@gmail.com>
2023-08-23 21:15:52 -0400

    fist commi

--- resources/views/listado.blade.php
+++ resources/views/listado.blade.php
@@ -8,15 +8,14 @@
     @vite('resources/css/app.css')
     <title>Document</title>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"/>
     <link href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="h-screen flex justify-center items-center  bg-gray-300 text-gray-600 font-semibold">
         <div class="bg-white h-fit rounded-md p-4 w-full max-w-4xl flex flex-col items-center ">
             <div class="flex w-full justify-between">
-                <h1 class="text-gray-800 rounded-md">Lsita de Usuarios</h1>
+                <h1 class="text-gray-800 rounded-md">Lista de Usuarios</h1>
                 <a class="bg-blue-950 p-2 block h-fit text-white rounded-lg" href="./">Agregar</a>
             </div>
             <div class=" w-full">
@@ -55,14 +54,17 @@
     </div>
 
     </form>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
-
-    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-
-    <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+    
     <script type="text/javascript">
         $(function() {
-            var table = $('#usuarios').DataTable();
+            var table = $('#usuarios').DataTable({
+              
+            });
         });
     </script>
 
